220db9ac57ed2e203a622376d9a6fc6b2c8ba1bb
Garett Breen <garett.breen@gmail.com>
2023-10-17 21:20:38 -0600

    Update

--- EsriGeometryCreate/EsriGeometryCreateApp.swift
+++ EsriGeometryCreate/EsriGeometryCreateApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 @main
 struct EsriGeometryCreateApp: App {
     init() {
-        ArcGISEnvironment.apiKey = APIKey("AAPKac6a3feb405249228dc5f0ec94295b4fjzXLQhtJ-g8xCdYOShcTMWfyYBkxY0nNa4wRkV2k1fhcKOps2yz0oHkhuOvIKc0r")
+        ArcGISEnvironment.apiKey = APIKey("")
     }
     
     var body: some SwiftUI.Scene {
